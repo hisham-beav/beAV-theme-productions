cefa4ed316e4dd1a501156b70d33f07a2dcc122c
hisham-beav <mh@beav.ae>
2025-12-04 12:39:42 +0400

    UPDATE NAVIGATION LOCALE

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,9 +1,9 @@
 import { createNavigation } from 'next-intl/navigation';
 
-export const locales = ['en', 'ar'] as const;
+export const locales = ['en-US', 'ar'] as const;
 export type Locale = (typeof locales)[number];
 export const localePrefix = 'as-needed';
-export const defaultLocale = 'en';
+export const defaultLocale = 'en-US';
 
 export const { Link, redirect, usePathname, useRouter } =
   createNavigation({ locales, localePrefix, defaultLocale });
